93bbab82a62722a2d6401e6b122056077e8aff23
FongFox <phong.tgn.coder@gmail.com>
2026-06-28 16:02:48 +0700

    codecrafters submit [skip ci]

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -15,7 +15,8 @@ public class Shell {
                 String[] arguments = Arrays.copyOfRange(words, 1, words.length);
                 switch (command) {
                     case "echo" -> handleEcho(arguments);
-                    case "exit" -> handleExit(scanner);
+                    case "type" -> handleType(arguments);
+                    case "exit" -> handleExit();
                     default -> handleUnknown(input);
                 }
             }
@@ -26,11 +27,24 @@ public class Shell {
     private void handleEcho(String[] arguments) {
         System.out.println(String.join(" ", arguments));
     }
-    private void handleExit(Scanner scanner) {
+
+    private void handleExit() {
         System.exit(0);
     }
+
     // Todo next stage
-    private void handleType(String command) {}
+    private void handleType(String[] arguments) {
+        String command = arguments[0];
+        switch (command) {
+            case "echo", "type", "exit" -> {
+                System.out.printf("%s is a shell builtin%n", command);
+                break;
+            }
+            default -> {
+                System.out.printf("%s: not found%n", command);
+            }
+        }
+    }
 
     // --- fallback ---
     private void handleUnknown(String input) {
