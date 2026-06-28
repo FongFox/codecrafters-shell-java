dbbe138bcf2edcb0901eb1d97fe7d10d4e32d79a
FongFox <phong.tgn.coder@gmail.com>
2026-06-28 14:51:59 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
             System.out.print("$ ");
             String input = sc.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }
             System.out.printf("%s: command not found%n", input);
         }
+        sc.close();
     }
 }
