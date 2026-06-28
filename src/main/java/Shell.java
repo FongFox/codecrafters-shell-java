a771098027fef4d6e5db023e81e86db8d8afcef2
FongFox <phong.tgn.coder@gmail.com>
2026-06-28 16:12:17 +0700

    codecrafters submit [skip ci]

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -1,24 +1,27 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Shell {
+    private static final Set<String> BUILTINS = Set.of("echo", "type", "exit");
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                continue;
+            }
+
             String[] words = input.split(" ");
-            if (words.length == 0) {
-                System.out.print("$ ");
-            } else {
-                String command = words[0];
-                String[] arguments = Arrays.copyOfRange(words, 1, words.length);
-                switch (command) {
-                    case "echo" -> handleEcho(arguments);
-                    case "type" -> handleType(arguments);
-                    case "exit" -> handleExit();
-                    default -> handleUnknown(input);
-                }
+            String command = words[0];
+            String[] arguments = Arrays.copyOfRange(words, 1, words.length);
+            switch (command) {
+                case "echo" -> handleEcho(arguments);
+                case "type" -> handleType(arguments);
+                case "exit" -> handleExit();
+                default -> handleUnknown(input);
             }
         }
     }
@@ -35,14 +38,10 @@ public class Shell {
     // Todo next stage
     private void handleType(String[] arguments) {
         String command = arguments[0];
-        switch (command) {
-            case "echo", "type", "exit" -> {
-                System.out.printf("%s is a shell builtin%n", command);
-                break;
-            }
-            default -> {
-                System.out.printf("%s: not found%n", command);
-            }
+        if (BUILTINS.contains(command)) {
+            System.out.printf("%s is a shell builtin%n", command);
+        } else {
+            System.out.printf("%s: not found%n", command);
         }
     }
 
