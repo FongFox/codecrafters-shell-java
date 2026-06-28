ae5ae64a3869da70f67194e15b1822da9501843d
FongFox <phong.tgn.coder@gmail.com>
2026-06-28 15:48:24 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,5 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.print("$ ");
-            String input = sc.nextLine();
-            String[] words = input.split(" ");
-            if (words.length == 0) {
-                System.out.print("$ ");
-            } else {
-                String command = words[0];
-                String[] arguments = Arrays.copyOfRange(words, 1, words.length);
-                if (command.equals("echo")) {
-                    System.out.println(String.join(" ", arguments));
-                } else if (command.equals("exit")) {
-                    sc.close();
-                    return;
-                } else {
-                    System.out.printf("%s: command not found%n", input);
-                }
-            }
-        }
+        new Shell().run();
     }
 }
---
+++ src/main/java/Shell.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Shell {
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            String[] words = input.split(" ");
+            if (words.length == 0) {
+                System.out.print("$ ");
+            } else {
+                String command = words[0];
+                String[] arguments = Arrays.copyOfRange(words, 1, words.length);
+                switch (command) {
+                    case "echo" -> handleEcho(arguments);
+                    case "exit" -> handleExit(scanner);
+                    default -> handleUnknown(input);
+                }
+            }
+        }
+    }
+
+    // --- built-in commands ---
+    private void handleEcho(String[] arguments) {
+        System.out.println(String.join(" ", arguments));
+    }
+    private void handleExit(Scanner scanner) {
+        System.exit(0);
+    }
+    // Todo next stage
+    private void handleType(String command) {}
+
+    // --- fallback ---
+    private void handleUnknown(String input) {
+        System.out.printf("%s: command not found%n", input);
+    }
+}
