911d04598889fd4de34ebe97bc665d3cb246bf17
FongFox <phong.tgn.coder@gmail.com>
2026-06-28 15:16:26 +0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,11 +7,21 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = sc.nextLine();
-            if (input.equals("exit")) {
-                break;
+            String[] words = input.split(" ");
+            if (words.length == 0) {
+                System.out.print("$ ");
+            } else {
+                String command = words[0];
+                String[] arguments = Arrays.copyOfRange(words, 1, words.length);
+                if (command.equals("echo")) {
+                    System.out.println(String.join(" ", arguments));
+                } else if (command.equals("exit")) {
+                    sc.close();
+                    return;
+                } else {
+                    System.out.printf("%s: command not found%n", input);
+                }
             }
-            System.out.printf("%s: command not found%n", input);
         }
-        sc.close();
     }
 }
